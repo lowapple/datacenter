bc42c563d99cf4e94f55ef0a9828b3a255f86f5d
Junseo <lowapple99@gmail.com>
2018-12-01 17:35:04 +0900

    공공 데이터 ServiceKey 변경

--- visitkor.py
+++ visitkor.py
@@ -1,16 +1,16 @@
 import requests
 
-base_url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon'
+base_url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode'
 # base_url = 'http://api.visitkorea.or.kr/openapi/service'
-service_key = "uyhEtW8fzsb3e9mz1%2BDoRmdabPTkWTLFRaTUE5Sq4yGK7r7Rm1ZGbSIujcoc96reP5k8YlYKdasBpWbHuSlG0g%3D%3D"
-service_key = service_key.encode('utf-8')
+service_key = "gRUX5P33koKONMFfxvMtowyPSjacHjsgcJtR85r0iDBAQ20pLll2ns%2BoOzeHhKKfZ9tvSZR2RkUuzQLx1KA1YQ%3D%3D"
+# service_key = service_key.encode('utf-8')
 
 params = {
     'ServiceKey': service_key,
-    'numOfRows': '10',
-    'pageNo': '1',
+    'numOfRows': 10,
+    'pageNo': 1,
     'MobileOS': 'ETC',
-    'MobileApp' : 'AppTest',
+    'MobileApp' : 'DayTour',
     'areaCode' : 1,
     '_type' : 'json'
 
