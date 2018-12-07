2106947955f8363e48ab3fa1d6cd3270b4f84589
Junseo <lowapple99@gmail.com>
2018-12-08 00:34:55 +0900

    add route(/search_area)

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+# -- coding: utf-8 --
+
+from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +10,27 @@ app = Flask(__name__)
 def index():
     return 'Welcome DayTour data center'
 
+
+@app.route('/search_area', methods=['POST'])
+def search_area():
+    # Request
+    import requests
+    import config
+    base_url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode'
+    params = {
+        'ServiceKey': config.service_key,
+        'numOfRows': request.form['rows'],
+        'pageNo': request.form['page'],
+        'MobileOS': 'ETC',
+        'MobileApp': 'DayTour',
+        '_type': 'json'
+    }
+    res = requests.get(url=base_url, params=params)
+
+    # Data Parsing
+    data = json.loads(res.text)
+    data = json.dumps(data['response']['body']['items']['item'])
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
